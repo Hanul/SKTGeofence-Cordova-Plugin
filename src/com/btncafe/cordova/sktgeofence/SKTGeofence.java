899681aab524e40e4f793e7e3f5a04f10fa30adf
Hanul <mr@hanul.co>
2014-11-03 23:36:28 +0900

    no message

--- src/com/btncafe/cordova/sktgeofence/SKTGeofence.java
+++ src/com/btncafe/cordova/sktgeofence/SKTGeofence.java
@@ -73,8 +73,10 @@ public class SKTGeofence {
 		}
 
 		@Override
-		public void onSynchronizeStatusChanged(GeoFenceSyncState arg0, boolean arg1) {
-			// ignore.
+		public void onSynchronizeStatusChanged(GeoFenceSyncState state, boolean arg1) {
+			if (connectedListener != null && state.type == GeoFenceSyncState.syncFinished) {
+				connectedListener.onConnected();
+			}
 		}
 
 		@Override
@@ -85,10 +87,6 @@ public class SKTGeofence {
 
 			// Web-Poc와 단말간의 데이터 동기화
 			agentManager.syncWDB(appData);
-
-			if (connectedListener != null) {
-				connectedListener.onConnected();
-			}
 		}
 	};
 
